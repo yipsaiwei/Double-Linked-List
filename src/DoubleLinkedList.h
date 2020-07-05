0758aa80af77fd750fd2c2de03efca7986957200
yipsaiwei <yipsaiwei@gmail.com>
2020-07-06 00:34:03 +0800

    Double Linked List

--- src/DoubleLinkedList.h
+++ src/DoubleLinkedList.h
@@ -3,15 +3,15 @@
 
 typedef	struct	ListItem	ListItem;
 struct ListItem{
-	ListItem	*next;
-	ListItem	*prev;
+	ListItem		*next;
+	ListItem		*prev;
 	int			data;
 };
 
 typedef	struct	DoubleLinkedList	DoubleLinkedList;
 struct	DoubleLinkedList{
-	ListItem	*head;
-	ListItem	*tail;
+	ListItem		*head;
+	ListItem		*tail;
 	int			count;
 };
 

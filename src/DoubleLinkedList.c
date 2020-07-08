36e643cb719ca918896c8993c41eb400f1469a16
yipsaiwei <65482031+yipsaiwei@users.noreply.github.com>
2020-07-09 00:11:53 +0800

    Update DoubleLinkedList.c

--- src/DoubleLinkedList.c
+++ src/DoubleLinkedList.c
@@ -8,25 +8,25 @@
     return the number of items in the double linked list
 */
 int	addItemToHead(ListItem* Item, DoubleLinkedList* accList){
-	int count;
-	if(accList->head==NULL)
+  int count;
+  if(accList->head==NULL)
   {
     accList->head=Item;
     accList->tail=Item;
-		accList->count=1;
-		count=accList->count;
-		return count;
-	}
-	else
-	{
-		Item->next=accList->head;
-		accList->head->prev=Item;
-		accList->head=Item;
-		Item->prev=NULL;
-		accList->count++;
-		count=accList->count;
-		return count;		
-	}
+    accList->count=1;
+    count=accList->count;
+    return count;
+  }
+  else
+  {
+    Item->next=accList->head;
+    accList->head->prev=Item;
+    accList->head=Item;
+    Item->prev=NULL;
+    accList->count++;
+    count=accList->count;
+    return count;		
+  }
 }
 
 /* This function is to add items to the tail of double linked list.
@@ -35,25 +35,25 @@ int	addItemToHead(ListItem* Item, DoubleLinkedList* accList){
     return the number of items in the double linked list
 */
 int addItemToTail(ListItem* Item , DoubleLinkedList* accList){
-	int count;
-	if(accList->head==NULL)
-	{
-		accList->head=Item;
-		accList->tail=Item;
-		accList->count=1;
-		count=accList->count;
-		return count;
-	}
-	else
-	{
-		Item->prev=accList->tail;
-		accList->tail->next=Item;
-		Item->next=NULL;
-		accList->tail=Item;
-		accList->count++;
-		count=accList->count;
-		return count;	
-	}
+  int count;
+  if(accList->head==NULL)
+  {
+    accList->head=Item;
+    accList->tail=Item;
+    accList->count=1;
+    count=accList->count;
+    return count;
+  }
+  else
+  {
+    Item->prev=accList->tail;
+    accList->tail->next=Item;
+    Item->next=NULL;
+    accList->tail=Item;
+    accList->count++;
+    count=accList->count;
+    return count;	
+  }
 }
 
 /* This function is to remove items from the head of given double linked list.
@@ -62,27 +62,27 @@ int addItemToTail(ListItem* Item , DoubleLinkedList* accList){
 */
 ListItem* removeItemFromHead(DoubleLinkedList* accList)
 {
-	ListItem	*returnedItem={NULL, NULL, 0};
-	if(accList->head==NULL)
-		return NULL;
-	else if (accList->head==accList->tail)
-	{
-		returnedItem=accList->head;
-		accList->head=NULL;
-		accList->tail=NULL;
-		accList->count--;
-		return returnedItem;
-	}
-	else
-	{
-		returnedItem=accList->head;
-		accList->head->next->prev=NULL;
-		accList->head=accList->head->next;
-		returnedItem->next=NULL;
-		returnedItem->prev=NULL;
-		accList->count--;
-		return	returnedItem;
-	}
+  ListItem	*returnedItem={NULL, NULL, 0};
+  if(accList->head==NULL)
+  return NULL;
+  else if (accList->head==accList->tail)
+  {
+    returnedItem=accList->head;
+    accList->head=NULL;
+    accList->tail=NULL;
+    accList->count--;
+    return returnedItem;
+  }
+  else
+  {
+    returnedItem=accList->head;
+    accList->head->next->prev=NULL;
+    accList->head=accList->head->next;
+    returnedItem->next=NULL;
+    returnedItem->prev=NULL;
+    accList->count--;
+    return	returnedItem;
+  }
 }
 
 /* This function is to remove items from the tail of given double linked list.
@@ -91,25 +91,25 @@ ListItem* removeItemFromHead(DoubleLinkedList* accList)
 */
 ListItem* removeItemFromTail(DoubleLinkedList* accList)
 {
-	ListItem	*returnedItem={NULL, NULL, 0};
-	if(accList->head==NULL)
-		return NULL;
-	else if (accList->head==accList->tail)
-	{
-		returnedItem=accList->tail;
-		accList->head=NULL;
-		accList->tail=NULL;
-		accList->count--;
-		return returnedItem;
-	}
-	else
-	{
-		returnedItem=accList->tail;
-		accList->tail->prev->next=NULL;
-		accList->tail=accList->tail->prev;
-		returnedItem->next=NULL;
-		returnedItem->prev=NULL;
-		accList->count--;
-		return	returnedItem;
-	}
+  ListItem	*returnedItem={NULL, NULL, 0};
+  if(accList->head==NULL)
+    return NULL;
+  else if (accList->head==accList->tail)
+  {
+    returnedItem=accList->tail;
+    accList->head=NULL;
+    accList->tail=NULL;
+    accList->count--;
+    return returnedItem;
+  }
+  else
+  {
+    returnedItem=accList->tail;
+    accList->tail->prev->next=NULL;
+    accList->tail=accList->tail->prev;
+    returnedItem->next=NULL;
+    returnedItem->prev=NULL;
+    accList->count--;
+    return	returnedItem;
+  }
 }
